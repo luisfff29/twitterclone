b7dfffe82f558de13763f834689b08021637de69
luisfff29 <luisfff29@gmail.com>
2020-05-27 01:18:42 -0500

    make model for tweets

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from twitteruser.models import CustomUser
+from django.utils import timezone
+
 
 # Create your models here.
+class TweetMessage(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user
