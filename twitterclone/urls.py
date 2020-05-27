c821442f7637b93a20d5af8e26dbb2f06fef1df3
luisfff29 <luisfff29@gmail.com>
2020-05-27 00:23:26 -0500

    add nav bar

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -19,6 +19,6 @@ from twitteruser import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='homepage'),
     path('', include('authentication.urls')),
 ]
