52655c13d219934d0253632e5e5d4c42af6ea1c3
luisfff29 <luisfff29@gmail.com>
2020-05-26 20:48:41 -0500

    custom user added

--- twitteruser/admin.py
+++ twitteruser/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from twitteruser.models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+admin.site.register(CustomUser, UserAdmin)
