4dd672848607bc720a958f661cc8adbf0a12b8fa
luisfff29 <luisfff29@gmail.com>
2020-05-26 21:21:18 -0500

    display login page w/ forms

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from authentication.forms import LoginForm
 
 # Create your views here.
+
+
+def index(request):
+    form = LoginForm()
+    return render(request, 'authentication/index.html', {'form': form})
