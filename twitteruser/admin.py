ccced9ca687312306e1ed4ba04fc40c9161f356b
luisfff29 <luisfff29@gmail.com>
2020-05-28 09:36:58 -0500

    add ManyToManyField in CustomUser model

--- twitteruser/admin.py
+++ twitteruser/admin.py
@@ -3,5 +3,19 @@ from twitteruser.models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
 
-# Register your models here.
-admin.site.register(CustomUser, UserAdmin)
+# From https://stackoverflow.com/questions/48011275/custom-user-model-fields-abstractuser-not-showing-in-django-admin
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,        # original form fieldsets, expanded
+        (                            # new fieldset added on to the bottom
+            'Custom Field Heading',  # group heading of your choice
+            {
+                'fields': (
+                    'following',
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- twitteruser/models.py
+++ twitteruser/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    following = models.ManyToManyField('self')
     pass
