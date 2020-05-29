021ea2488878c1476a6c642e564ee0e8b29929e0
luisfff29 <luisfff29@gmail.com>
2020-05-29 11:05:30 -0500

    create notification model

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from twitteruser.models import CustomUser
+from tweet.models import TweetMessage
+
 
 # Create your models here.
+class NotificationModel(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(TweetMessage, on_delete=models.CASCADE)
+    viewed = models.BooleanField()
