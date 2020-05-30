acf13a94b11610f21d7ebe84ee173e164bde7d94
luisfff29 <luisfff29@gmail.com>
2020-05-29 19:55:07 -0500

    add base.html to nofitication page

--- notification/views.py
+++ notification/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from notification.models import NotificationModel
+from tweet.models import TweetMessage
+from twitteruser.models import CustomUser
 
 
 # Create your views here.
 def notificationview(request):
-    return render(request, 'notification/notification.html')
+    num_tweets = TweetMessage.objects.filter(
+        user=CustomUser.objects.get(username=request.user.username)).count()
+    return render(request, 'notification/notification.html', {'num_tweets': num_tweets})
