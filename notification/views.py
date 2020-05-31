08377552eb31594feebc46e0973942d9e60a7b9c
luisfff29 <luisfff29@gmail.com>
2020-05-30 23:27:37 -0500

    show notifications only once

--- notification/views.py
+++ notification/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from notification.models import NotificationModel
 from tweet.models import TweetMessage
 from twitteruser.models import CustomUser
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def notificationview(request):
     num_tweets = TweetMessage.objects.filter(
         user=CustomUser.objects.get(username=request.user.username)).count()
