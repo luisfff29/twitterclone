73e1f0fd4f8185b1809a0e582bf9dd53f99fce55
luisfff29 <luisfff29@gmail.com>
2020-06-07 01:12:39 -0500

    change tweet compose & detail view into CBV

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,6 +3,6 @@ from tweet import views
 
 
 urlpatterns = [
-    path('compose/', views.tweetview, name='compose'),
-    path('<int:id>/', views.tweetdetail, name='tweet'),
+    path('compose/', views.TweetView.as_view(), name='compose'),
+    path('<int:id>/', views.TweetDetail.as_view(), name='tweet'),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -5,13 +5,26 @@ from twitteruser.models import CustomUser
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from notification.models import NotificationModel
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 import re
 
 
 # Create your views here.
-@login_required
-def tweetview(request):
-    if request.method == 'POST':
+class TweetView(LoginRequiredMixin, View):
+    def get(self, request):
+        num_tweets = TweetMessage.objects.filter(
+            user=CustomUser.objects.get(username=request.user.username)).count()
+        num_notif = NotificationModel.objects.filter(
+            user=request.user, viewed=False).count()
+        form = TweetForm()
+        return render(request, 'tweet/tweet.html', {
+            'form': form,
+            'num_tweets': num_tweets,
+            'num_notif': num_notif
+        })
+
+    def post(self, request):
         form = TweetForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
@@ -31,35 +44,25 @@ def tweetview(request):
 
         return HttpResponseRedirect(reverse('homepage'))
 
-    num_tweets = TweetMessage.objects.filter(
-        user=CustomUser.objects.get(username=request.user.username)).count()
-    num_notif = NotificationModel.objects.filter(
-        user=request.user, viewed=False).count()
-    form = TweetForm()
-    return render(request, 'tweet/tweet.html', {
-        'form': form,
-        'num_tweets': num_tweets,
-        'num_notif': num_notif
-    })
 
+class TweetDetail(View):
+    def get(self, request, id):
+        tweet = TweetMessage.objects.get(id=id)
+        num_tweets = TweetMessage.objects.filter(user=tweet.user).count()
+        profile_user = tweet.user
+        try:
+            num_notif = NotificationModel.objects.filter(
+                user=request.user, viewed=False).count()
+            following = profile_user in CustomUser.objects.get(
+                username=request.user.username).following.all()
+        except TypeError:
+            num_notif = 0
+            following = False
 
-def tweetdetail(request, id):
-    tweet = TweetMessage.objects.get(id=id)
-    num_tweets = TweetMessage.objects.filter(user=tweet.user).count()
-    profile_user = tweet.user
-    try:
-        num_notif = NotificationModel.objects.filter(
-            user=request.user, viewed=False).count()
-        following = profile_user in CustomUser.objects.get(
-            username=request.user.username).following.all()
-    except TypeError:
-        num_notif = 0
-        following = False
-
-    return render(request, 'tweet/detail.html', {
-        'tweet': tweet,
-        'following': following,
-        'num_tweets': num_tweets,
-        'profile_user': profile_user,
-        'num_notif': num_notif
-    })
+        return render(request, 'tweet/detail.html', {
+            'tweet': tweet,
+            'following': following,
+            'num_tweets': num_tweets,
+            'profile_user': profile_user,
+            'num_notif': num_notif
+        })
