c5cb74ebe9f40faaafbcb9e272cabfebc6e97b1a
luisfff29 <luisfff29@gmail.com>
2020-09-20 16:27:06 -0500

    update tweet card and compose tweet

--- tweet/forms.py
+++ tweet/forms.py
@@ -2,4 +2,10 @@ from django import forms
 
 
 class TweetForm(forms.Form):
-    body = forms.CharField(max_length=140, widget=forms.Textarea())
+    body = forms.CharField(max_length=140, widget=forms.Textarea(
+        attrs={
+            'class': 'form-control my-3 bg-tweet text-white',
+            'placeholder': "What's happening?",
+            'rows': '5'
+        }
+    ))
