1e9a936e7b68212bcfbd76d58a1268bdbe355f2d
luisfff29 <luisfff29@gmail.com>
2020-09-23 05:31:59 -0500

    modify signup and login pages

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,11 +2,39 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control bg-tweet text-white border-0',
+            'placeholder': 'Username'
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control bg-tweet text-white border-0',
+            'placeholder': 'Password',
+            'type': 'password'
+        }
+    ))
 
 
 class SignupForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput())
-    password = forms.CharField(widget=forms.PasswordInput())
-    email = forms.EmailField()
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control bg-tweet text-white border-0',
+            'placeholder': 'Username'
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control bg-tweet text-white border-0',
+            'placeholder': 'Password',
+            'type': 'password'
+        }
+    ))
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={
+            'class': 'form-control bg-tweet text-white border-0',
+            'placeholder': 'Email',
+            'type': 'email'
+        }
+    ))
