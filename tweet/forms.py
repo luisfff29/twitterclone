2d2bee207aae8000d10bec0b62b66f3fa193f4c0
luisfff29 <luisfff29@gmail.com>
2020-09-30 17:57:56 -0500

    display number of comments on every tweet

--- tweet/forms.py
+++ tweet/forms.py
@@ -15,5 +15,5 @@ class CommentForm(forms.Form):
     body = forms.CharField(max_length=140, widget=forms.Textarea(attrs={
         'class': 'form-control bg-tweet text-white',
         'placeholder': 'Write a comment...',
-        'rows': '1',
+        'rows': '2',
     }))
