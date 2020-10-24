b0472ef3e0dfeaa71241b6e2594adb4ac0b1109f
luisfff29 <luisfff29@gmail.com>
2020-10-24 11:28:10 -0500

    track media just when debug is true

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 handler404 = 'authentication.views.handler404'
 handler500 = 'authentication.views.handler500'
