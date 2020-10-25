6c9dd2104ece45ba17dd443473cc8ad040d9fca4
luisfff29 <luisfff29@gmail.com>
2020-10-25 07:18:52 -0500

    display tweet comment model on admin & fix date url on comments

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from tweet.models import TweetMessage
+from tweet.models import TweetMessage, TweetComment
 
 
 # Register your models here.
 admin.site.register(TweetMessage)
+admin.site.register(TweetComment)
